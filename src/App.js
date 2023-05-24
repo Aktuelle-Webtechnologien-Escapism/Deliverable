69ecfca333a058e5cf1992d49289426d1753f1fd
Yejay <demirkan.yejay@gmail.com>
2023-05-24 17:51:06 +0200

    testing deployment

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
-
+// Test
 // import MainPage from './components/MainPage';
 import ToDoList from './components/ToDoList';
 
