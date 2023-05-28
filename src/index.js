25c27b538c74a68e88b4f8b66d529d31c2197ef8
s86197 <vioannidou3@gmail.com>
2023-05-28 20:56:23 +0200

    testing_yaml

--- src/index.js
+++ src/index.js
@@ -21,14 +21,27 @@
  */
 
 
-import React from 'react';
-import Sentry from "./sentry";
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import { createRoot } from "react-dom/client";
+import React from "react";
+import * as Sentry from "@sentry/react";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <Sentry.ErrorBoundary fallback="An error has occurred.">
-      <App />
-    </Sentry.ErrorBoundary>
-  );
+Sentry.init({
+  dsn: "https://74b1b4ccb33e471cb4e38d8edca06515@o4505256071004160.ingest.sentry.io/4505263003729920",
+  integrations: [
+    new Sentry.BrowserTracing({
+      // Set `tracePropagationTargets` to control for which URLs distributed tracing should be enabled
+      tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+    }),
+    new Sentry.Replay(),
+  ],
+  // Performance Monitoring
+  tracesSampleRate: 1.0, // Capture 100% of the transactions, reduce in production!
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<App />)
--- src/sentry.js
+++ src/sentry.js
@@ -1,24 +1,24 @@
-import * as Sentry from "@sentry/react";
+// import * as Sentry from "@sentry/react";
 
-Sentry.init({
-    dsn: "https://5e9f29936cea48658e72ee50836b4df2@o4505256071004160.ingest.sentry.io/4505256076574720",
-    integrations: [
-      new Sentry.BrowserTracing({
-        // Set `tracePropagationTargets` to control for which URLs distributed tracing should be enabled
-        tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
-      }),
-      new Sentry.Replay()
-    ],
+// Sentry.init({
+//     dsn: "https://74b1b4ccb33e471cb4e38d8edca06515@o4505256071004160.ingest.sentry.io/4505263003729920",
+//     integrations: [
+//       new Sentry.BrowserTracing({
+//         // Set `tracePropagationTargets` to control for which URLs distributed tracing should be enabled
+//         tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+//       }),
+//       new Sentry.Replay()
+//     ],
   
-    // Set tracesSampleRate to 1.0 to capture 100%
-    // of transactions for performance monitoring.
-    // We recommend adjusting this value in production
-    tracesSampleRate: 1.0,
+//     // Set tracesSampleRate to 1.0 to capture 100%
+//     // of transactions for performance monitoring.
+//     // We recommend adjusting this value in production
+//     tracesSampleRate: 1.0,
   
-    // Capture Replay for 10% of all sessions,
-    // plus for 100% of sessions with an error
-    replaysSessionSampleRate: 0.1,
-    replaysOnErrorSampleRate: 1.0,
-  });
+//     // Capture Replay for 10% of all sessions,
+//     // plus for 100% of sessions with an error
+//     replaysSessionSampleRate: 0.1,
+//     replaysOnErrorSampleRate: 1.0,
+//   });
   
-  export default Sentry;
+//   export default Sentry;
